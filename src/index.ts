53a08353e17c4dd5d825453213c86186fe22745a
mattia lau <mattialau@jwemac01.local>
2020-11-04 14:55:44 +0800

    expose telegraf typing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./telegram.module";
 export * from "./telegram.service";
 export * from "./types";
-export * from "telegraf/typings"
+export * from "telegraf/typings/context";
+export * from "telegraf/typings/telegram-types";
+export * from "telegraf"
